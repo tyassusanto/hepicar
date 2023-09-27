f7e9ebe94b6e9ce5e8c47cec385075871d9a70d6
tyassusanto <nasusayt@gmail.com>
2023-09-27 22:38:02 +0700

    first commit

---
+++ be/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = 3300;
+
+
+app.listen(PORT, () => {
+    console.log(`server is running port : ${PORT}`)
+});

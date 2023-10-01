f007eef58d30cd635a1a6daf42f2281848462d26
tyassusanto <nasusayt@gmail.com>
2023-10-01 14:24:06 +0700

    minor change

--- be/src/routes/services.js
+++ be/src/routes/services.js
@@ -3,8 +3,8 @@ const route = express.Router();
 const servicesController = require('../controllers/services');
 const common = require('../common/common')
 
-route.get('/', servicesController.searchName) // done
-route.post('/add', servicesController.addService) // done
-route.delete('/delete/:id', servicesController.deleteService) // done
-route.put('/update/:id', servicesController.updateService)
+route.get('/', common.auth, servicesController.searchName) // done
+route.post('/add', common.auth, servicesController.addService) // done
+route.delete('/delete/:id', common.auth, servicesController.deleteService) // done
+route.put('/update/:id', common.auth, servicesController.updateService)
 module.exports = route
